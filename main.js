4ae4da978232af2717f406cdb9dcba4bfca8472b
IJQShrestha <shrestha.iq@obmontessori.edu.ph>
2025-03-25 08:14:45 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function verify() {
+	var age = document.getElementById("age").value;
+
+	if (age >= 18) {
+		window.alert("You are eligble to vote!");
+	}
+	else {
+		window.alert("GET OUT OF HEREEE!");
+	}
+}
